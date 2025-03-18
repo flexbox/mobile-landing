e55889ae35f754b104cb0ed6cdb67cc34f1d6874
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2025-03-18 14:27:53 +0100

    fix: 🐛 brand typo

--- components/brand/BrandTypography.tsx
+++ components/brand/BrandTypography.tsx
@@ -4,18 +4,16 @@ import { theme } from '@/constants/theme';
 
 export function BrandTypography() {
   return (
-    <View className="my-6">
-      <Text tx="brand.typography.title" variant="heading2" className="mb-4" />
+    <View className="my-12">
+      <Text tx="brand.typography.title" variant="heading2" className="mb-8" />
       <ScrollView>
         {Object.entries(theme.typography).map(([key, style]) => (
           <View key={key} className="mb-6">
-            <Text tx={`brand.components.typography.items.${key}.name`} variant="body" className="mb-1" />
             <Text
-              className="text-gray-900 mb-1"
+              className="text-gray-900 my-2"
               style={{ fontSize: style.fontSize, fontWeight: style.fontWeight }}
-            >
-              {`brand.components.typography.items.${key}.name`}
-            </Text>
+              tx={`brand.components.typography.items.${key}.name`}
+            />
             <Text tx={`brand.components.typography.items.${key}.description`} variant="caption" />
           </View>
         ))}
